c8047b7365a1dcad1e10c5e06fe0487e75d25778
Vera <1102472738@qq.com>
2018-09-19 22:49:22 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端300毫秒click事件延迟解决方案
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 1像素边框解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router: router,
   components: { App },
   template: '<App/>'
 })
